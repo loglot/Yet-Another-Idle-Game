c9fdebdcca61b431da2d8839c650aa06c8f331aa
loglot <88983354+loglot@users.noreply.github.com>
2023-08-31 14:19:41 -0500

    QoL change, No More Manually Zooming In For Your Browser

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
       const ctx = canvas.getContext("2d");
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+const originalWidth = canvas.width;
+const originalHeight = canvas.height;
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
       window.requestAnimationFrame(gameLoop)
 
@@ -28,7 +29,31 @@
         while(true) {
             keyManager.doActionsFromKeyInput();
             keyManager.onTick();
+            resizeCanvasForWindowSize()
             draw.Game()
             await misc.sleep(1000/60)
         }
     }
+
+    function resizeCanvasForWindowSize() {
+
+      // Get the current window dimensions
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+  
+      // Calculate the desired width and height based on the window's dimensions
+      const desiredWidth = windowWidth;
+      const aspectRatio = originalWidth / originalHeight;
+      const desiredHeight = desiredWidth / aspectRatio;
+  
+      // Set the canvas element's width and height
+      canvas.width = desiredWidth;
+      canvas.height = desiredHeight;
+  
+      // Resize the canvas drawing area to maintain the aspect ratio
+      scaleX = (desiredWidth / originalWidth);
+      scaleY = (desiredHeight / originalHeight);
+  
+      // Apply the scaling transformation to maintain the aspect ratio
+      ctx.setTransform(scaleY, 0, 0, scaleX, 0, 0)
+  }
