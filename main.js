7b9909cf7677c104804b8e0977f754de08e8caad
LogLot <prattlog3@gmail.com>
2024-09-06 21:48:03 -0500

    added settings!

--- main.js
+++ main.js
@@ -10,11 +10,49 @@ var game = new Game()
     function gameLoop() {
       game.keyMan.update()
       game.display.Game()
-      game.player.update()
-      if(game.keyMan.wasKeyJustPressed("KeyW") && game.MainMenu){
+      if(!game.MainMenu){
+        game.player.update()
+      }
+      if(game.keyMan.wasKeyJustPressed("KeyW") && game.MainMenu && game.MenuState == "main"){
         game.MainMenu = false
         game.player.grow(70)
+      }else if(game.keyMan.wasKeyJustPressed("KeyS") && game.MainMenu && game.MenuState == "main"){
+        game.MenuState = "settings"
+      }else if(game.keyMan.wasKeyJustPressed("KeyS") && game.MainMenu && game.MenuState == "settings"){
+        game.MenuState = "main"
+      }else if(game.keyMan.wasKeyJustPressed("ArrowUp") && game.MainMenu && game.MenuState == "settings"){
+        game.SettingSelect -= 1
+      }else if(game.keyMan.wasKeyJustPressed("ArrowDown") && game.MainMenu && game.MenuState == "settings"){
+        game.SettingSelect += 1
+      }else if(game.keyMan.wasKeyJustPressed("ArrowLeft") && game.MainMenu && game.MenuState == "settings"){
+        if(game.SettingSelect == 0){
+          game.Style = !game.Style
+        }
+        if(game.SettingSelect == 1){
+          game.Grow = !game.Grow
+        }
+        if(game.SettingSelect == 2){
+          game.SpeedSelect--
+          if(game.SpeedSelect < 0){
+            game.SpeedSelect = 4
+          }
+        }
+      }else if(game.keyMan.wasKeyJustPressed("ArrowRight") && game.MainMenu && game.MenuState == "settings"){
+          if(game.SettingSelect == 0){
+            game.Style = !game.Style
+          }
+          if(game.SettingSelect == 1){
+            game.Grow = !game.Grow
+          }
+          if(game.SettingSelect == 2){
+            game.SpeedSelect++
+            if(game.SpeedSelect > 4){
+              game.SpeedSelect = 0
+            }
+          }
+        
       }
+      console.log(game.MenuState)
       requestAnimationFrame(gameLoop)
     }
     requestAnimationFrame(gameLoop)
