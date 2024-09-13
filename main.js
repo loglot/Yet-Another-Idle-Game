c87140307874c67b6e4202d6ce8ae1eb8fa5c932
loglot <88983354+loglot@users.noreply.github.com>
2024-09-13 17:23:19 +0000

    working on it, broken rn

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ var game = new Game()
       if(game.keyMan.wasKeyJustPressed("KeyW") && game.State == "main"){
         game.State = "game"
         game.soundMenu.volume = .15
+        game.Start()
         if(game.Audios){
           game.soundMenu.play()
         }
