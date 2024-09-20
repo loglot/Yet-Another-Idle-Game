35619ed266aa4f7181b46435cd8b5ee4a7c98454
loglot <88983354+loglot@users.noreply.github.com>
2024-09-20 12:57:45 +0000

    should be locked to a nice crisp smooth 60 fps now

--- main.js
+++ main.js
@@ -4,10 +4,29 @@ import { Game } from "./lib/imports.js";
 
 var game = new Game()
 
+// const perfectFrameTime = 1000 / 60;
+// var fps = 0
+// let deltaTime = 0;
+// let lastTimestamp = 0;
+// let lastFrameMenu = true
 
+let msPrev = window.performance.now()
+const targetFPS = 60
+const msPerFrame = 1000 / targetFPS
 
 
     function gameLoop() {
+      requestAnimationFrame(gameLoop)
+
+      const msNow = window.performance.now()
+      const msPassed = msNow - msPrev
+    
+      if (msPassed < msPerFrame) return
+
+      const excessTime = msPassed % msPerFrame
+      msPrev = msNow - excessTime
+
+
       game.keyMan.update()
       game.display.Game()
       game.menu.tick()
@@ -32,7 +51,6 @@ var game = new Game()
         game.State = "game"
             game.display.leave = true
       }
-      requestAnimationFrame(gameLoop)
     }
     requestAnimationFrame(gameLoop)
 
